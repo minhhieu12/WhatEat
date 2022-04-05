99a77cd6a570b7b16eaba01c092d679417fa7a7a
minhhieu12 <hieuleggo1280@gmail.com>
2022-04-05 23:51:38 +0700

    hieu update

--- Backend/API/config/database.js
+++ Backend/API/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-        'url': 'mongodb://localhost:27017/dbtest'
+        'url': 'mongodb+srv://hieutm1280:hieu123leggo456@cluster0.zoxl3.mongodb.net/WHATEAT?retryWrites=true&w=majority'
 };
